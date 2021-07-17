be93626571bd6b750194227a33ffe29727d3b91d
arrypotter1 <hello2arvind@gmail.com>
2021-07-17 16:18:25 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<style>
+body
+{
+	color : black;
+}
+img {
+	align : center;
+}
+.fullscreenDiv {
+    background-color: #808080;
+	
+    width: 100%;
+    height: auto;
+    bottom: 0px;
+    top: 0px;
+    left: 0;
+    position: absolute;
+}
+
+.center {
+    position: absolute;
+	background-color: #ffffff;
+
+	
+    border: 1px solid blue;
+    width: 80%;
+	text-align: left;
+    height: 95%;
+    top: 5%;
+  left : 10%;
+    margin-top: -25px;
+    margin-left: -50px;
+}
+.imgcenter {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 20%;
+}
+
+</style>
+<div class="fullscreenDiv">
+<div class="center">
+<h1>Hello AWS!</h1>
+<p>I was tasked with an assignment to build a AMI. What I wanted to do was to think beyond, to think bold and solve a global problem. The problem I choose was based on <a href="https://www.financialexpress.com/industry/sme/8-crore-retail-wholesale-traders-incur-rs-6-25-lakh-cr-business-loss-due-to-covid-restrictions/2248611/" target="_blank"	>this article</a>  </br>
+
+<img  src ="https://img.etimg.com/thumb/msid-76097015,width-300,imgsize-339811,,resizemode-4,quality-100/kirana.jpg"  class ="imgcenter"></img>
+<p> India was one of the most severly affected countries due to COVID and <font color="blue"><b>80 MN Retailers in India and 90 BN loss</b></font> today have incurred losses, with several of them having to shut down. A major concern with them was they did not have a "Digital Platform". When the pandemic stuck, they had no option but to shut down </p>
+
+<p> My goal was - what if there was a E-Commerce solution that allowed these small retailers to stay relevant and not having to shut down their operations. 
+
+I worked on the solution, learnt several concepts, failed, learnt and improve my solution</br>
+1. Packer deployed, Docker enabled E-Commerce built on PHP (had mysql, was not able to deploy it due to performance issues)</br>
+2. CIS Security 1 enabled </br>
+3. Built to run on t2.micro </br>
+I have done testing on it for security, accessiblity and performance
+</p>
+
+
+<form action="main.php">
+    <input type="submit" value="Go to ECOMMERCE" class = "imgcenter"/>
+</form>
+</div>
+</div>
